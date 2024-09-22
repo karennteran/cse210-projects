56117f20e9e25587d137f2cf858ba1a50475ebf2
karennteran <terankarenn@gmail.com>
2024-09-22 10:24:49 -0400

    Finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a series of numbers, type 0 when finished.");
+        
+        while (true)
+        {
+            number = int.Parse(Console.ReadLine());
+
+            if (number == 0)
+
+            {
+                Console.WriteLine("You finished!!");
+                break;
+            }
+
+            numbers.Add(number);
+
+        }
     }
 }
