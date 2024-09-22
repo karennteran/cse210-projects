c8bcb196d94c2a5e41c0184e5457fa4980f09571
karennteran <terankarenn@gmail.com>
2024-09-21 23:33:15 -0400

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int guess = 0;
+
+
+        while (guess != number)
+        {
+            Console.WriteLine("what is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        
+
+            if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it, congratulations");
+            }
+        }
     }
 }
