f14f69d2dcfe848efb4a4c33f54793925e87be49
karennteran <terankarenn@gmail.com>
2024-09-29 14:58:46 -0400

    learning 2 finished

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,11 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Job job1 = new Job ()
+        Job job1 = new Job ();
         job1._jobTitle = "software Engineer";
         job1._company = "Microsoft";
         job1._startYear = 2019;
-        Job1._endYear= 2022;
+        job1._endYear= 2022;
 
         Job job2 = new Job();
         job2._jobTitle = "Manager";
@@ -17,7 +17,7 @@ class Program
         job2._endYear = 2023;
 
         Resume myResume = new Resume();
-        myResume._name "Allison Rose";
+        myResume._name = "Allison Rose";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
